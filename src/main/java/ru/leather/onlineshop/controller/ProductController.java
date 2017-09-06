72f84d9962017138d859ad94e2b442944db84864
Anton <mikhaylov86@gmail.com>
2017-09-06 23:44:57 +0300

    add Dispatcher and Controller Product

---
+++ src/main/java/ru/leather/onlineshop/controller/ProductController.java
@@ -0,0 +1,28 @@
+package ru.leather.onlineshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.leather.onlineshop.service.ProductService;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    @Qualifier(value = "productServiceImpl")
+    public void setUserService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.GET)
+    public String listUsers(Model model){
+        model.addAttribute("allProduct", productService.getAll());
+
+        return "product";
+    }
+}
--- src/main/java/ru/leather/onlineshop/repository/ProductRepository.java
+++ src/main/java/ru/leather/onlineshop/repository/ProductRepository.java
@@ -3,8 +3,10 @@ package ru.leather.onlineshop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.leather.onlineshop.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
     @Query("select b from Product b where b.name = :name")
     Product findByName(@Param("name") String name);
