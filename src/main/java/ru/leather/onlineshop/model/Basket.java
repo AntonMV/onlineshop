b2739b05de7a74a186500bc64990ed82bae3d94c
Anton <mikhaylov86@gmail.com>
2017-09-07 22:38:56 +0300

    add model Basket

--- src/main/java/ru/leather/onlineshop/model/Basket.java
+++ src/main/java/ru/leather/onlineshop/model/Basket.java
@@ -1,4 +1,35 @@
 package ru.leather.onlineshop.model;
 
 public class Basket {
+
+    private Integer productId;
+
+    private String productName;
+
+    private Integer productQuantity;
+
+    private Double productPrice;
+
+    public Basket(Integer productId, String productName, Integer productQuantity, Double productPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productQuantity = productQuantity;
+        this.productPrice = productPrice;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
 }
