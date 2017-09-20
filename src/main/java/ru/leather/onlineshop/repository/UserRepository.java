6e3afc72cf0d1709d721c3fa906fa7618cb30339
arcon <mikhaylov86@gmail.com>
2017-09-20 16:54:21 +0300

    edit UserRepo

--- src/main/java/ru/leather/onlineshop/repository/UserRepository.java
+++ src/main/java/ru/leather/onlineshop/repository/UserRepository.java
@@ -8,4 +8,7 @@ import ru.leather.onlineshop.model.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("select b from User b where b.email = :email")
     User findByName(@Param("email") String email);
+
+//    @Query("select b from User b where b.email = ?1")
+//    User findByName(String email);
 }
